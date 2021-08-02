114d56dec19d0ad79d620b4ed791597095f408fc
Kleir Miranda <kleirmiranda@gmail.com>
2021-08-02 14:40:11 -0400

    changed dayListItem back to DayListItem line 5

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import "components/DayListItem.scss";
 import classNames from "classnames";
 
-export default function dayListItem(props) {
+export default function DayListItem(props) {
   const dayClass = classNames({
     "day-list__item": true,
     "day-list__item--selected": props.selected,
