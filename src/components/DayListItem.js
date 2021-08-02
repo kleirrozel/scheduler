363ad55681d18473d08963b5b41a7a1f33b522ca
Kleir Miranda <kleirmiranda@gmail.com>
2021-08-02 13:52:17 -0400

    Created object for the classNames function called dayClass

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,21 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
 
 export default function DayListItem(props) {
+  let dayClass = classNames({
+    "day-list__item": true,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li
+      className = {dayClass}
+      onClick= {() => props.setDay(props.name)}
+    >
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{props.spots}</h3>
     </li>
   );
 }
