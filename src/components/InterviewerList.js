143a3e0d4ccbead07132e8ded9e93b0198475af5
Kleir Miranda <kleirmiranda@gmail.com>
2021-08-03 19:08:26 -0400

    Refactored Controlled List

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,7 +3,7 @@ import "components/InterviewerList.scss";
 import InterviewerListItem from "components/InterviewerListItem";
 
 export default function InterviewerList(props) {
-  const { interviewers, setInterviewer } = props
+  const { interviewers, onChange, value } = props
 
   return (
     <section className="interviewers">
@@ -14,8 +14,8 @@ export default function InterviewerList(props) {
             InterviewerListItem key = {interviewer.id}
             name = {interviewer.name}
             avatar = {interviewer.avatar}
-            selected = {interviewer.id === props.interviewer}
-            setInterviewer = {event => setInterviewer(interviewer.id)}
+            selected = {interviewer.id === value }
+            setInterviewer = {event => onChange(interviewer.id)}
           / >
         )}
       </ul>
