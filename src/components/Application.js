9b4bd33150abafc5b50a504c1643275609d8ccdc
Kleir Miranda <kleirmiranda@gmail.com>
2021-08-04 21:36:09 -0400

    Removed hardcoded days array and added axios/fetch to access days api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,5 @@
-// import React from "react";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 
@@ -73,26 +73,16 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/days").then(response => {
+      setDays(response.data)
+      console.log("THIS IS THE RESPONSE.DATA   ", response.data);
+    });
+  }, [])
 
   return (
     <main className="layout">
@@ -106,7 +96,9 @@ export default function Application(props) {
             <DayList
               days={days}
               day={day}
-              setDay={setDay}
+              setDay={day => {
+                setDay(day);
+              }}
             />
           </nav>
           <img
