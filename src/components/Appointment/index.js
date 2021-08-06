27745e2a619ee71f5c7cf4f70f07ebe8961dca7b
Kleir Miranda <kleirmiranda@gmail.com>
2021-08-06 18:08:21 -0400

    Added error handling for saving and deleting

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -22,6 +22,8 @@ export default function Appointment(props) {
   const DELETING = "DELETING";
   const CONFIRM = "CONFIRM";
   const EDIT = "EDIT";
+  const ERROR_SAVE = "ERROR_SAVE";
+  const ERROR_DELETE ="ERROR_DELETE";
 
   const { mode, transition, back } = useVisualMode(
     interview ? SHOW : EMPTY
@@ -34,12 +36,18 @@ export default function Appointment(props) {
     };
 
     transition(SAVING);
-    bookInterview(id, interview).then(() => transition(SHOW));
+
+    bookInterview(id, interview)
+    .then(() => transition(SHOW))
+    .catch(error => transition(ERROR_SAVE, true));
   };
 
   const cancel = () => {
-    transition(DELETING);
-    cancelInterview(id).then(() => transition(EMPTY));
+    transition(DELETING, true);
+
+    cancelInterview(id)
+    .then(() => transition(EMPTY))
+    .catch(error => transition(ERROR_DELETE, true));
   };
 
   return (
@@ -78,6 +86,15 @@ export default function Appointment(props) {
           onCancel={() => back()}
           onSave={save}
         />
+      )}
+      {mode === ERROR_SAVE && (
+        <Error 
+          message ="Could not save appointment."
+          onClose={() => back()} />)}
+      {mode === ERROR_DELETE && (
+        <Error 
+          message ="Could not cancel appointment."
+          onClose={() => back()} />
       )}
     </article>
   )
